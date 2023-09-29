4f47e2c5df7ecf4468886be6cb7b3229b57eb1a9
Abdo Mahmoud <UniqDigiR@gmail.com>
2023-09-29 11:24:34 -0400

    Update rounded_corners_rectangle_1.rs

--- tiny_skia_examples/rounded_corners_rectangle_1/rounded_corners_rectangle_1.rs
+++ tiny_skia_examples/rounded_corners_rectangle_1/rounded_corners_rectangle_1.rs
@@ -1,16 +1,36 @@
-//This example was written by Abdo Mahmoud (Unique-Digital-Resources): https://github.com/Unique-Digital-Resources
+// NOTES:
+//==============================================================================
+//- This example was written by Abdo Mahmoud (Unique-Digital-Resources): https://github.com/Unique-Digital-Resources
 
+//- The following libraries were used in this example:
+//       tiny-skia : https://github.com/RazrFalcon/tiny-skia
+//       Go to the links to find out the original authors of each library, the license and more examples if availabe.
+
+//- You do not have to mention this source and its author or provide credit to the author when you use it for any use (personally,
+//   commercially, etc.), but it will be appreciated by you, and mentioning this source and the project:
+//   https://github.com/Unique-Digital-Resources/Learn-graphics-for-theoretical-gui/tree/main
+//   will help increase the number of potential contributors to the project, 
+//   and help More programmers and learners in their learning
+//==============================================================================
+
+// Import libraries
 use tiny_skia::{Paint, Pixmap, FillRule, Transform, Path, PathBuilder};
 
 
 
 fn main() {
+    
+    // Create paint (the color/shader used to fill drawed shape) then create pixmap.
+    // Draw the shape using rounded_corners_rectangle_draw_1 function on the pixmap. 
+    // Go to line 39 - 74 to understand how rounded_corners_rectangle_draw_1 works.
+
+    //==============================================================================
+
     let mut paint = Paint::default();
     paint.set_color_rgba8(0, 127, 0, 200);
     paint.anti_alias = true;
 
     let mut pixmap = Pixmap::new(1000, 1000).unwrap();
-
 
 
     pixmap.fill_path(
@@ -22,6 +42,8 @@ fn main() {
     );
 
     pixmap.save_png("rounded_corners_rectangle.png").unwrap();
+
+    //==============================================================================
 }
 
 
@@ -30,6 +52,8 @@ fn main() {
 
 fn rounded_corners_rectangle_draw_1(x:f32,y:f32,w:f32,h:f32,mut r:f32) -> Path
 {
+    //  Set limits for r, such that the sides of the curves do not intersect each other.
+    //==============================================================================
     if h>w{
         if r > 0.24 * w{
             r = 0.24 * w
@@ -45,9 +69,12 @@ fn rounded_corners_rectangle_draw_1(x:f32,y:f32,w:f32,h:f32,mut r:f32) -> Path
             r = 0.24 * w
         }
     }
+    //==============================================================================
 
-    print!("r = {}",r);
 
+    // Cubic curves used as corners of the rectangle.
+    // See 
+    //==============================================================================
     let mut pb = PathBuilder::new();
     pb.move_to(x+r, y);
     pb.line_to(w-r, y);
@@ -59,8 +86,14 @@ fn rounded_corners_rectangle_draw_1(x:f32,y:f32,w:f32,h:f32,mut r:f32) -> Path
     pb.line_to(x, y+r);
     pb.cubic_to(x,y+(r/2 as f32),x+(r/2 as f32),y, x+r, y);
     pb.close();
+    //==============================================================================
     
-    
+    // This function returns the shape as a Path to be a parameter for pixmap.fill_path function in line 30.
     let path = pb.finish().unwrap();
     return path;
 }
+
+// Usefull resources:
+// https://www.desmos.com/calculator/qddv8ytxpf (Interactive cubic curves to understand how to work with them).
+// https://docs.rs/tiny-skia/latest/tiny_skia/ (tiny-skia docs).
+// https://github.com/RazrFalcon/tiny-skia/tree/master/examples (original examples).
