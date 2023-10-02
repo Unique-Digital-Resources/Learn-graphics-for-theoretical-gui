fe96993dccae651b2e5418493a7b4c2d00b4adf1
Abdo Mahmoud <UniqDigiR@gmail.com>
2023-10-02 07:19:07 -0400

    Update graph_1.rs

--- tiny_skia_examples/graph_1/graph_1.rs
+++ tiny_skia_examples/graph_1/graph_1.rs
@@ -1,11 +1,26 @@
-//This example was written by Abdo Mahmoud (Unique-Digital-Resources): https://github.com/Unique-Digital-Resources
+// NOTES:
+//==============================================================================
+//- This example was written by Abdo Mahmoud (Unique-Digital-Resources): https://github.com/Unique-Digital-Resources
 
+//- The following libraries were used in this example:
+//       tiny-skia : https://github.com/RazrFalcon/tiny-skia
+//       Go to the links to find out the original authors of each library, the license and more examples if availabe.
+
+//- You do not have to mention this source and its author or provide credit to the author when you use it for any use (personally,
+//   commercially, etc.), but it will be appreciated by you, and mentioning this source and the project:
+//   https://github.com/Unique-Digital-Resources/Learn-graphics-for-theoretical-gui/tree/main
+//   will help increase the number of potential contributors to the project, 
+//   and help More programmers and learners in their learning
+//==============================================================================
+
+// Import libraries
 use tiny_skia::{Paint, Pixmap, Stroke, FillRule, Transform, PathBuilder};
 
 
 
 fn main() {
-
+    // We create a list containing data that will be represented in the graph, such that each item in the list:
+    // (value in x, value in y)
     let points_list:[(f32,f32); 6] = [(1.0,1.0), (2.0,2.0), (4.0,2.0), (6.0,2.0), (7.0, 5.0), (9.0,1.0)];
 
     let pixmap = Pixmap::new(1000, 1000).unwrap();
@@ -15,7 +30,7 @@ fn main() {
 }
 
 fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut pixmap:Pixmap){
-
+    // x and y are position of the graph, w is width, h is heigth, xco is x axis scale, yco is y axis scale
     let mut point_paint = Paint::default();
     point_paint.set_color_rgba8(50, 127, 150, 255);
     point_paint.anti_alias = true;
@@ -28,6 +43,7 @@ fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut 
     circle_paint.set_color_rgba8(200, 100, 100, 255);
     circle_paint.anti_alias = true;
 
+    // Draw x and y axis of the graph
     let mut line = PathBuilder::new();
     line.move_to(x, y);
     line.line_to(x, y+h);
@@ -37,9 +53,8 @@ fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut 
     line_stroke.width = 10.0;
     pixmap.stroke_path(&line_path, &line_paint, &line_stroke, Transform::identity(), None);
     
-
+    // Draw y scale in equal separation
     for i in 0..yco{
-        // println!("resullllt {}", (y+h)/i as f32)
         let mut ycoline = PathBuilder::new();
         ycoline.move_to(x, (y+h)-h/yco as f32 * (i+1) as f32);
         ycoline.line_to(x-50.0, (y+h)-h/yco as f32 * (i+1) as f32);
@@ -48,7 +63,7 @@ fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut 
         ycostroke.width = 5.0;
         pixmap.stroke_path(&ycopath,&line_paint,&ycostroke, Transform::identity(),None);
     }
-
+    // Draw x scale in equal separation
     for i in 0..xco{
         let mut xcoline = PathBuilder::new();
         xcoline.move_to(x + w/xco as f32 * (i+1) as f32, y+h);
@@ -58,7 +73,7 @@ fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut 
         xcostroke.width = 5.0;
         pixmap.stroke_path(&xcopath,&line_paint,&xcostroke, Transform::identity(),None)
     }
-
+    // For each point, draw a line its start point is the current point and its end point is the next point, to represent the relation line on the graph
     for i in 0..points.len()-1{
         let mut line = PathBuilder::new();
         line.move_to(x + w/xco as f32 * points[i].0, (y+h)-h/yco as f32 *(points[i].1));
@@ -68,7 +83,7 @@ fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut 
         line_stroke.width = 5.0;
         pixmap.stroke_path(&line_path, &point_paint,&line_stroke, Transform::identity(),None)
     }
-
+    // For each point, draw a circle on the point's position
     for i in 0..points.len(){
         let mut point_circle = PathBuilder::new();
         point_circle.push_circle(x + w/xco as f32 * points[i].0, (y+h)-h/yco as f32 *(points[i].1), 10.0);
@@ -78,3 +93,7 @@ fn graph(x:f32, y:f32, w:f32, h:f32, xco:i32, yco:i32, points:&[(f32,f32)], mut 
 
     pixmap.save_png("graph.png").unwrap();
 }
+
+// Usefull resources:
+// https://docs.rs/tiny-skia/latest/tiny_skia/ (tiny-skia docs).
+// https://github.com/RazrFalcon/tiny-skia/tree/master/examples (original examples).
